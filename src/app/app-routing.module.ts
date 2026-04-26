df9714f7a6ab513a8a47013e33e1b0f371f338ea
Your Name <you@example.com>
2026-04-26 14:10:37 -0300

    feat: integra layout com sistema de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './core/layout/layout.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'veiculos',
+        loadChildren: () =>
+          import('./modules/veiculos/veiculos.module').then(m => m.VeiculosModule)
+      },
+      { path: '', redirectTo: 'veiculos', pathMatch: 'full' }
+    ]
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
